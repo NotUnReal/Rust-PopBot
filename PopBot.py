326728b1d44fb5875e5d2b6e2d6b245002f2e130
UnReal <not.the.unreal.deal@gmail.com>
2020-12-20 01:43:06 -0500

    Update PopBot.py
    Wrote the whole popBot

--- PopBot.py
+++ PopBot.py
@@ -1,1 +1,50 @@
+import discord
+import arrow
+import requests
+import aiohttp
+from discord.ext import commands, tasks
 
+# -----------------------------------------------------
+#-------------------- START CONFIG --------------------
+# -----------------------------------------------------
+
+discordBotToken = "NjY1NjE0ODc2NTY3MTQyNDEx.XhoMKQ.dnEjxYpf4jpKaYeElaX7ZIIQ5lU"
+battleMetricsServerID = 2569573
+
+# -----------------------------------------------------
+#--------------------- END CONFIG ---------------------
+# -----------------------------------------------------
+
+intents = discord.Intents(messages=True)
+client = commands.Bot(command_prefix="-",intents=intents)
+client.remove_command('help')
+
+@client.event
+async def on_ready():
+    print(f"[{arrow.now().format('YYYY-MM-DD HH:mm:ss')}] Bot successfully started\n")
+    change_status.start()
+
+@tasks.loop(seconds=60)
+async def change_status():
+    serverData = await makeWebRequest(f"https://api.battlemetrics.com/servers/{battleMetricsServerID}")
+    if serverData == None:
+        return
+
+    serverPlayers = serverData['data']['attributes']['players']
+    serverMaxPlayers = serverData['data']['attributes']['maxPlayers']
+    serverQueue = serverData['data']['attributes']['details']['rust_queued_players']
+
+    if serverQueue > 0:
+        await client.change_presence(activity=discord.Game(f"{serverPlayers}/{serverMaxPlayers} Queue {serverQueue}"))
+    else:
+        await client.change_presence(activity=discord.Game(f"{serverPlayers}/{serverMaxPlayers}"))
+
+async def makeWebRequest(URL):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(URL) as preJSData:
+            if preJSData.status == 200:
+                return await preJSData.json()
+            else:
+                print(f"BattleMetrics Error [Code {preJSData.status}]")
+
+client.run(discordBotToken)
